752b7bafc2dffc86aa76602142569b1a66eea847
Asad Khan <yousafzaigaming@gmail.com>
2023-09-24 03:13:18 +0530

    Prototype-1

---
+++ main.js
@@ -0,0 +1,33 @@
+import './style.css'
+
+
+let todoList= [];
+
+let taskInput=document.getElementById("tasks");
+let addTaskBtn=document.getElementById("addTask")
+let taskList=document.getElementById("task-list")
+
+addTaskBtn.addEventListener("click",()=> {
+    let task=tasksInput.Value.trim();
+
+    if (task !== ""){
+        let todoItem={
+            text: task
+        }
+        
+    todoList.push(todoItem)
+
+    let taskradio = document.createElement('input');
+    taskradio.type = 'radio';
+    taskradio.name = 'todo';
+    taskradio.value = 'task';
+
+    const taskLabel = document.createElement('label');
+    taskLabel.textContent = task;
+
+    taskList.appendChild(taskradio);
+    taskList.appendChild(taskLabel);
+
+    taskInput.value = '';
+}
+});
